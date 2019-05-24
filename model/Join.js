c357f2d56b169d68d1192236f3f85f2bcc16081c
Dereck San Miguel <derecksanmiguel@dhcp-v124-032.mobile.uci.edu>
2019-05-23 20:44:02 -0700

    added Join and User tables

---
+++ model/Join.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize')
+const connection = require('../config')
+
+class Join extends Sequelize.Model { }
+
+Join.init({
+    hunger: Sequelize.INTEGER,
+    hungerT:Sequelize.INTEGER,
+    exhaust: Sequelize.INTEGER,
+    exhaustT:Sequelize.INTEGER,
+    boredom: Sequelize.INTEGER,
+    boredomT:Sequelize.INTEGER,
+    affection: Sequelize.INTEGER,
+    affectionT:Sequelize.INTEGER,
+    anger: Sequelize.INTEGER,
+    angerT:Sequelize.INTEGER,
+    sadness: Sequelize.INTEGER,
+    sadnessT:Sequelize.INTEGER
+
+}, {
+        sequelize: connection,
+        modelName: 'join'
+    })
+
+module.exports = Join
