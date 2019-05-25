c342b7e5a67298d2000c93fffc2d142f04e694fd
Poweruperman <byung.david.yoo@gmail.com>
2019-05-25 15:30:57 -0700

    config duplicate removed

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app = express()
 const { join } = require('path')
-require("dotenv").config()
 const PORT = process.env.PORT || 3000
-require('dotenv').config()
+require("dotenv").config()
 
 app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended : true }))
