a4014337a0e45cab5f15bcf5cdf73709c71207fe
ByungJae David Yoo <lifeisgood1004@gmail.com>
2019-05-24 22:21:54 -0700

    functions layout initialized

---
+++ pokedex/src/component/action/action.js
@@ -0,0 +1,23 @@
+// Write a react page and functions to run actions
+
+// Feeding
+// Sleep
+// Train
+// Park 
+// Pet
+// Play 
+// Clean
+// Ignore
+
+import React, { Component } from 'react'
+
+class action extends Component {
+    render () {
+        return (
+            <>
+                {/* All the action HTML will go in here */}
+                {/* Functions will have to be written outside of Render() */}
+            </>
+        )
+    }
+}
