56d1882bf39065532b6357bc14cde409445253d4
haog29 <hgagaring@yahoo.com>
2019-06-01 11:03:41 -0700

    adds more to join

--- pokedex/src/components/pokeSel/pokeSel.js
+++ pokedex/src/components/pokeSel/pokeSel.js
@@ -1,10 +1,15 @@
+import './App.css'
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class PokeSel extends Component {
     render () {
         return (
             <>
+
                 <h1> This is PokeSel Page</h1>
+
+
             </>
         )
     }
