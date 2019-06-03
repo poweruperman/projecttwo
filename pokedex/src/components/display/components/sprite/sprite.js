8973481a98f215f49e9a7f52e3b73e79cd1f3164
Poweruperman <byung.david.yoo@gmail.com>
2019-06-02 19:40:35 -0700

    spirte, tstat, background, display halfway

--- pokedex/src/components/display/components/sprite/sprite.js
+++ pokedex/src/components/display/components/sprite/sprite.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react'
 
 class Sprite extends Component {
     state = {
-
+        spriteURL : ''
+    }
+    componentDidMount = () => {
+        setTimeout(() => {
+            this.props.pokeState ? this.setState({ spriteURL : this.props.front_img}) : this.setState({ spriteURL : this.props.back_img})
+        }, 3000)
     }
     render () {
-        const { pokeState, front_img, back_img} = this.props
+        const { pokeState, front_img, back_img, isReady } = this.props
+        const { spriteURL } = this.state
         return (
             <>
-            {pokeState ? <img src={front_img} alt='front'/> : <img src={back_img} alt='back' />}
+            {isReady ? <img src={spriteURL} alt='spriteImg'/> : ''}
             </>
         )
     }
